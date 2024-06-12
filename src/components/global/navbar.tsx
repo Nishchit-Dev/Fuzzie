f1403c6e36bd8dbd905148d776bde2b52b87e3e5
Nishchit-Dev <nishchitpatel84@gmail.com>
2024-06-12 15:01:36 -0500

    added theme & navbar

---
+++ src/components/global/navbar.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+type Props = {};
+
+const Navbar = async (props: Props) => {
+  return (
+    <header className="fixed right-0 left-0 top-0 p-4 bg-black/40 backdrop-blur-lg z-[100] flex items-center border-b-[1px] border-neutral-900 justify-between">
+      <aside className="flex items-center gap-[2px]">
+        <p className="text-3xl font-bold"> Fuzzie</p>
+      </aside>
+    </header>
+  );
+};
+
+export default Navbar;
